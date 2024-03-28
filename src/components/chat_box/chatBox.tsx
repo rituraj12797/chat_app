04a5fca934c17f25e39c777e6e350727345e5a3e
rituraj12797 <alchemyrecloak@gmail.com>
2024-03-28 18:15:00 +0530

    infnite scrolling with virtualization added

--- src/components/chat_box/chatBox.tsx
+++ src/components/chat_box/chatBox.tsx
@@ -1,13 +1,17 @@
 
+import React from 'react';
 import  './chatBox.css'
 interface Props {
     className: string;
 }
-
-function ChatBox({className}:Props) {
-    return ( <div className={"chatbox h-[30px] w-[120px]  flex justify-center items-center "+className}>
-        <div className="messg h-[1005] w-[90%] rounded-md">
-          this is message 
+type ChatCompProps = {
+    className:string,
+    message:string
+}
+const ChatBox: React.FC<ChatCompProps> = ({className,message})=> {
+    return ( <div className={"chatbox h-[100px] w-[120px]  flex justify-center items-center "+className}>
+        <div className="messg h-[100%] w-[90%] rounded-md">
+          {message}
         </div>
         <div className='chat_directr h-[100%] w-[10%] bg-white'>
 
--- src/pages/chatRoom.tsx
+++ src/pages/chatRoom.tsx
@@ -10,7 +10,7 @@ import { Separator } from "@/components/ui/separator"
 import { io, Socket } from "socket.io-client";
 
 import ChatBox from "@/components/chat_box/chatBox"
-import { useEffect, useState } from "react"
+import { useCallback, useEffect, useRef, useState } from "react"
 
 import { Button } from "@/components/ui/button"
 import {
@@ -22,10 +22,62 @@ import {
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 
+import { Virtuoso } from 'react-virtuoso'
+
+import { useInfiniteQuery } from "@tanstack/react-query"
+import { useInView } from 'react-intersection-observer';
+
 const formSchema = z.object({
     textMessage: z.string().min(2).max(500),
 })
+
+
+type message = {
+    index: number,
+    chat: string
+}
+let chats: message[] = []
+for (let index = 0; index < 10; index++) {
+    chats.push({
+        index: index,
+        chat: `hi ${index}`
+    })
+}
+
+// lower the index latest is the chat 
+
+
+
+// when user scroll anobe we will need t use infinite query to fetch the data from redis 
 function ChatRoom() {
+
+
+
+
+    const virtuoso = useRef<any>(null)
+    const { ref, inView, entry } = useInView({
+        threshold: 0,
+    })
+
+    // const {
+    //     data,
+    //     fetchNextPage,
+    //     hasNextPage
+    // } = useInfiniteQuery({
+    //     queryKey: ['projects'],
+    //     queryFn: getUsers,
+    //     initialPageParam: 0,
+    //     getNextPageParam: (lastPage) => {
+    //         if (lastPage.prevOffset + 10 > lastPage.articleCount) {
+    //             return false;
+    //         }
+
+    //         return lastPage.prevOffset + 10;
+    //     },
+    // })
+    // const articles = data?.pages.reduce((acc, page) => {
+    //     return [...acc, ...page.articles]
+    // }, [])
 
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
@@ -35,10 +87,10 @@ function ChatRoom() {
     })
 
     // 2. Define a submit handler.
-    function submitHandler(values: z.infer<typeof formSchema>,type:string) {
+    function submitHandler(values: z.infer<typeof formSchema>, type: string) {
         // Do something with the form values.
         // ✅ This will be type-safe and validated.
-        sendMessage(values,type);
+        sendMessage(values, type);
     }
 
     const [socket, setSocket] = useState<Socket | null>(null);
@@ -70,14 +122,81 @@ function ChatRoom() {
         };
     }, [socket]);
 
+    type User = {
+        name: string;
+    }
 
-    {/* Duplicate messages are received due to adding the event listener multiple times on the client side, often within a React functional component, without removing previous listeners. To resolve, ensure setting up the listener once during component mount and implement cleanup using React's useEffect hook, preventing redundant event registrations and duplicate message reception. */ }
+    const [users, setUsers] = useState<User[]>([]); // Added type annotation for initial state
+
+    const loadMore = useCallback(() => {
+
+        return setTimeout(() => {
+            
+            setUsers((prevUsers) => [...prevUsers, ...generateUsers(10, prevUsers.length)]);
+        }, 500);
+
+    }, [setUsers]);
+
+   useEffect(()=>{
+    let v = 10
+   
+    virtuoso.current.scrollToIndex({
+        index:v,
+        align:"end",
+        behavior:"smooth"
+      });
+   },[users])
+
+    useEffect(() => {
+        const timeout = loadMore();
+        return () => clearTimeout(timeout);
+    }, []);
+
+    function generateUsers(count: number, start: number): User[] {
+        
+      
+        return Array.from({ length: count }, (_, i) => ({
+            name: `User ${start + i}`,
+        }));
+    }
+
+    useEffect(() => {
+      
+       if (inView) {
+        loadMore();
+        
+       }
+    }, [inView])
+   console.log(users)
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // function increaseChat() {
+    //     for (let index = chats.length; index < chats.length+10; index++) {
+    //         chats.push({
+    //             index:index,
+    //             chat:`hi ${index}`
+    //         })
+    //   }
+    // }
+
+    {/* Duplicate messages were received due to adding the event listener multiple times on the client side, often within a React functional component, without removing previous listeners. To resolve, ensure setting up the listener once during component mount and implement cleanup using React's useEffect hook, preventing redundant event registrations and duplicate message reception. */ }
     // useEffect(() => {
     //     setSocket(io("http://localhost:8000", {
     //         withCredentials: true
     //       }));
     //       console.log("socket");
-    // }, []);
+    // }, []);      
 
     // socket?.on("connect", () => {
     //     console.log("connected");
@@ -91,29 +210,42 @@ function ChatRoom() {
 
 
 
-    function sendMessage(message:z.infer<typeof formSchema>,type:string) {
+    function sendMessage(message: z.infer<typeof formSchema>, type: string) {
         console.log("this is the message from client", message);
-        if(type === "message"){
-            socket?.emit("message", message.textMessage);
+        if (type === "message") {
+            socket?.emit("message", { text: message.textMessage, type: "client" });
         }
-        else if(type === "broadcast"){
+        else if (type === "broadcast") {
             socket?.emit("broadcast", message.textMessage);
-       }
+        }
     }
     return (
         <div className="flex justify-center align-center relative flex-col w-[90%] gap-[20px] h-[100%]">
-            <ScrollArea className=" w-48 rounded-md relative border h-[0%] w-[100%]">
-                <div className="w-[100%] chat_messages">
-
-
-                    < div className="w-[100%]" >
-                        <ChatBox className={"left_chat"} />
-                        <Separator />
+            <ScrollArea className=" w-48 rounded-md relative border w-[100%] h-[100%]" >
+                <div className="w-[100%] chat_messages h-[100%]">
+                    < div className="w-[100%] h-[100%]" >
+                        <Virtuoso
+                            style={{ height: '650px', width: "100%", display: "flex" }}
+                            data={users}
+                            ref={virtuoso}
+                            initialTopMostItemIndex={users.length-1}
+                            itemContent={(index) => {
+                                index = users.length - 1 - index
+                                return <div>
+                                     {
+                                        (index===users.length - 1) && (<div ref={ref}>  </div>)
+                                     }
+                                     <div  className={`h-[80px] w-[200px]   ${index===(users.length-1) ? "bg-green-600":"bg-red-600"}`}>{users[index].name}</div>
+                                </div>
+                            }}
+                        />
+                        {/*  we were unable to reverse the virtuso so hat we will do is we will diaply the chats n reverse order at the client side and add the listener to 0th index to add more mesages or fetch infiinite query when the user reaches 0 it will fetch and then re assign all the index */}
                     </div>
+                    <div className="bottom_chat_indicator">{inView}</div>
 
                 </div>
             </ScrollArea>
-            <div className="h-[10%] w-[100%] bg-blue-100 flex justify-center items-center">
+            <div className="w-[100%] bg-blue-100 flex justify-center items-center">
 
                 <Form {...form}>
                     <form onSubmit={(event) => {
@@ -133,12 +265,12 @@ function ChatRoom() {
                         />
                         <Button type="submit" onClick={(event) => {
                             event.preventDefault();
-                            submitHandler(form.getValues(),"message")
+                            submitHandler(form.getValues(), "message")
                         }}>Submit </Button>
                         <Button type="submit" onClick={(event) => {
                             event.preventDefault();
-                            submitHandler(form.getValues(),"broadcast")
-                        }}>broadcast all</Button>
+                            submitHandler(form.getValues(), "broadcast")
+                        }}>broadcast room 1</Button>
                     </form>
                 </Form>
 
